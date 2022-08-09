90a2ca749ba2cfc3498c5655da5d7a6cd149802e
Hugo Gayoso <gayosohugo@gmail.com>
2022-08-09 10:33:34 -0300

    cleanned code

--- book_examples/11_UVM_Test/xcelium.d/run.lnx8664.20.09.d/.timestamp.ts
+++
@@ -1,6 +0,0 @@
-1660051578 /home/hugo.medeiros/Projects/uvm_primer_study/book_examples/11_UVM_Test/tinyalu_bfm.sv
-1660051578 /home/hugo.medeiros/Projects/uvm_primer_study/book_examples/11_UVM_Test/top.sv
-1659020332 /home/hugo.medeiros/Projects/uvmprimer/11_UVM_Test/top.sv
-1659020332 /home/hugo.medeiros/Projects/uvmprimer/11_UVM_Test/tinyalu_bfm.sv
-1660051578 /home/hugo.medeiros/Projects/uvm_primer_study/book_examples/11_UVM_Test/tinyalu_pkg.sv
-1659020332 /home/hugo.medeiros/Projects/uvmprimer/11_UVM_Test/tinyalu_pkg.sv
--- book_examples/11_UVM_Test/xcelium.d/run.lnx8664.20.09.d/worklib_-3.ts
+++
@@ -1,2 +0,0 @@
-1526655669 /home/EDA_tools/cds/XCELIUM2009/tools.lnx86/methodology/UVM/CDNS-1.1d/sv/src/uvm_pkg.sv
-1526655664 /home/EDA_tools/cds/XCELIUM2009/tools.lnx86/methodology/UVM/CDNS-1.1d/additions/sv/cdns_uvm_pkg.sv
